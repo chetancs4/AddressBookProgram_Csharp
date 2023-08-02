2b37608a8072606dc27e18e69877febdee957c99
Chetan <chetansadvelkar4@gmail.com>
2023-08-02 09:30:37 +0530

    AddressBook Program

---
+++ Contact.cs
@@ -0,0 +1,22 @@
+﻿namespace AddressBookProgram
+{
+    public class Contact
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName}\nAddress: {Address}, {City}, {State} {Zip}\nPhone: {PhoneNumber}\nEmail: {Email}";
+        }
+    }
+}
+
+
+
---
+++ Program.cs
@@ -0,0 +1,108 @@
+﻿namespace AddressBookProgram
+{
+
+    public class Program
+    {
+        static List<Contact> contacts = new List<Contact>();
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Address Book!");
+
+            bool isRunning = true;
+
+            while (isRunning)
+            {
+                Console.WriteLine("\nMenu:");
+                Console.WriteLine("1. Add Contact");
+                Console.WriteLine("2. Display All Contacts");
+                Console.WriteLine("3. Exit");
+
+                int choice = GetUserChoice();
+
+                switch (choice)
+                {
+                    case 1:
+                        AddContact();
+                        break;
+                    case 2:
+                        DisplayContacts();
+                        break;
+                    case 3:
+                        isRunning = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        break;
+                }
+            }
+        }
+
+        static int GetUserChoice()
+        {
+            Console.Write("Enter your choice: ");
+            int choice;
+            while (!int.TryParse(Console.ReadLine(), out choice))
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+                Console.Write("Enter your choice: ");
+            }
+            return choice;
+        }
+
+        static void AddContact()
+        {
+            Console.WriteLine("\nAdding a new contact:");
+            Console.Write("Enter First Name: ");
+            string firstName = Console.ReadLine();
+            Console.Write("Enter Last Name: ");
+            string lastName = Console.ReadLine();
+            Console.Write("Enter Address: ");
+            string address = Console.ReadLine();
+            Console.Write("Enter City: ");
+            string city = Console.ReadLine();
+            Console.Write("Enter State: ");
+            string state = Console.ReadLine();
+            Console.Write("Enter Zip: ");
+            string zip = Console.ReadLine();
+            Console.Write("Enter Phone Number: ");
+            string phoneNumber = Console.ReadLine();
+            Console.Write("Enter Email: ");
+            string email = Console.ReadLine();
+
+            Contact newContact = new Contact
+            {
+                FirstName = firstName,
+                LastName = lastName,
+                Address = address,
+                City = city,
+                State = state,
+                Zip = zip,
+                PhoneNumber = phoneNumber,
+                Email = email
+            };
+
+            contacts.Add(newContact);
+            Console.WriteLine("Contact added successfully!");
+        }
+
+        static void DisplayContacts()
+        {
+            if (contacts.Count == 0)
+            {
+                Console.WriteLine("No contacts found.");
+            }
+            else
+            {
+                Console.WriteLine("\nAll Contacts:");
+                foreach (Contact contact in contacts)
+                {
+                    Console.WriteLine(contact.ToString());
+                }
+            }
+        }
+    }
+}
+
+
+
